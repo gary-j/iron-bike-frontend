ccbed70299da5b9f0ba952f3b0afecbea71d5306
Léo Cheffi <contact@leocheffi.com>
2022-04-20 12:09:13 +0200

    scrollToUpPage

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProviderWrapper } from './context/auth.context';
 import { CartProviderWrapper } from './context/cart.context';
+import ScrollToTop from "./components/ScrollToTop"
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProviderWrapper>
         <CartProviderWrapper>
+      <ScrollToTop/>
           <App />
         </CartProviderWrapper>
       </AuthProviderWrapper>
